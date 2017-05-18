aea6724d9fe80d84ca92f3eb5315a4482723e173
RichardAlcalaC <jheranium@gmail.com>
2017-05-18 14:41:28 -0400

    Initial sharing of project

---
+++ TaskTest.java
@@ -0,0 +1,22 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class TaskTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class TaskTest
+{
+    @Test
+    public void createInstanceOfTask()
+    {
+        Task emptyTask = new Tas();
+    }
+
+}
